9da9be31ab30ed4e5fb57cf0e219dacbd60f3437
Alejandro Caceres Prado <acaceresprado@riotgames.com>
2025-10-08 10:54:31 +0100

    feat(sorting): added header component for sorting algorythms

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,24 @@
+import type {Algorithms} from "@/types";
+import {descriptions, type HeaderProps} from "@/components/Header/descriptions.ts";
+import React from "react";
+import {Chip, Stack} from "@mui/material";
+
+export const Header: React.FC<{selectedAlgorithm: Algorithms}> = ({selectedAlgorithm}) => {
+
+  const selected: HeaderProps = React.useMemo(() => descriptions[selectedAlgorithm], [selectedAlgorithm]);
+
+  return (
+  <Stack direction="column" alignItems="flex-start" gap={1}>
+    <h1>{selected.title}</h1>
+    <p>{selected.description}</p>
+    <Stack direction="row" spacing={2}>
+      <Chip label="Complexity" color="success" />
+      {Object.entries(selected.complexity).map(([scenario, complexity]) => (
+        <Chip label={`${scenario}: ${complexity}`}  />
+      ))}
+    </Stack>
+
+  </Stack>
+  );
+
+}
--- src/routes/algorithms/sorting.tsx
+++ src/routes/algorithms/sorting.tsx
@@ -1,9 +1,16 @@
 import { createFileRoute } from "@tanstack/react-router";
+import {Header} from "@/components/Header";
+import {Algorithms} from "@/types";
+
+
 
 export const Route = createFileRoute("/algorithms/sorting")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <div>Hello "/Algorithms/sorting"!</div>;
+  return <div>
+    <Header selectedAlgorithm={Algorithms.SelectionSort}/>
+
+  </div>;
 }
